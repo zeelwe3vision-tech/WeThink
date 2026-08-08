582d731c728a816f43acd232b9abe4c39961714a
Chetan Jain <chetanj.we3vision@gmail.com>
2026-08-08 15:12:32 +0530

    08.08.1
    Modify json file

--- app/client/src/features/auth/pages/Login.jsx
+++ app/client/src/features/auth/pages/Login.jsx
@@ -30,8 +30,8 @@ function Login() {
 
     try {
       const response = await axios.post(
-        // "http://localhost:5000/api/auth/login",
-        "https://wethink.onrender.com/api/auth/login",
+        "http://localhost:5000/api/auth/login",
+        // "https://wethink.onrender.com/api/auth/login",
         {
           email: email.trim(),
           password,
